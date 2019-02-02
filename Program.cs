617c5acb97ad6f589fb9216b40ea17f7a006f587
Guillermo del Cid <guillermodelcid@gmail.com>
2019-02-02 11:51:29 -0600

    Agregando linea de saludo

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Ejemplo
+{
+    class Program
+    {   
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Bienvenido al curso de .NET");
+            Console.WriteLine("Hello World!");
+            
+        }
+    }
+}
